8a9d903e101b4832701c3bc8dac13850f4841a3e
SeherKumsar <seherkumsar99@outlook.com>
2024-05-17 02:23:37 +0300

    REST API HTTP Request -> JSON

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,9 +1,11 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/"
+# endpoint = "https://httpbin.org/status/200/"
+endpoint = "https://httpbin.org/anything"
 
-get_response = requests.get(endpoint) # HTTP Request
+# get_response = requests.get(endpoint, data={"query":
+get_response = requests.get(endpoint, json={"query":
+    "Hello World"})
 # print(get_response) # print source code
 print(get_response.text) # raw text response
 
@@ -12,7 +14,11 @@ print(get_response.text) # raw text response
 # Phone -> Camera -> App -> API -> CAMERA
 # REST APIs -> Web Api
 # Http Request
-
-
-
-
+"""
+HTTP Request -> HTML
+REST API HTTP Request -> JSON (xml)
+JavaScript Object Nototion ~ Python Dict
+Jason, JSON
+"""
+print(get_response.json()) # raw text response
+print(get_response.status_code)
