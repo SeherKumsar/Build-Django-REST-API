bec186c40f8198511cf8057b1cf542ee8f68cea2
SeherKumsar <seherkumsar99@outlook.com>
2024-05-20 02:42:45 +0300

    Django Rest Framework Model Serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
 @api_view(["GET"]) 
 # @api_view dekoratörü, gelen isteği bir Django HttpRequest nesnesinden bir DRF Request nesnesine dönüştürür.
@@ -12,8 +13,15 @@ def api_home(request, *args, **kwargs):
     """
     # if request.method != "POST":
     #     return Response({"detail": "GET not allowed"}, status=405)
-    model_data = Product.objects.all().order_by("?").first()
+    # model_data = Product.objects.all().order_by("?").first()
+    # data = {}
+    # if model_data:
+    #     data = model_to_dict(model_data, fields=['id', 'title', 'price', 'sale_price'])
+    # return Response(data)
+
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    if instance:
+        # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
+        data = ProductSerializer(instance).data
     return Response(data)
