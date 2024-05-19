572339a86a3d1d8df1675a0c3c4e92fd7c136c9b
SeherKumsar <seherkumsar99@outlook.com>
2024-05-20 02:19:03 +0300

    Rest Framework View & Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,19 @@
-import json
 from django.forms.models import model_to_dict
-from django.http import JsonResponse, HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
 
+@api_view(["GET"]) 
+# @api_view dekoratörü, gelen isteği bir Django HttpRequest nesnesinden bir DRF Request nesnesine dönüştürür.
 def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
+    # if request.method != "POST":
+    #     return Response({"detail": "GET not allowed"}, status=405)
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
         data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-    return JsonResponse(data)
-
-        # print(data)
-        # data = dict(data)
-    #     json_data_str = json.dumps(data) # "dump string" (stringe dök)
-    # return HttpResponse(json_data_str, headers={"content-type": "application/json"})
+    return Response(data)
