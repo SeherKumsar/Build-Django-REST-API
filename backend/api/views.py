b9b59ea762fbb9ed763ab6dd359135bc953cbe9e
SeherKumsar <seherkumsar99@outlook.com>
2024-05-19 20:44:46 +0300

    Added byte string of JSON data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,13 @@
+import json
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
+
+    # request -> HttpRequest -> Django
+    # print(dir(request))
+    # request.body
+    body = request.body # byte string of JSON data
+    print(body)
     return JsonResponse(
         {
             "message":"Hi there, this is your Django API response!!"
--- py_client/basic.py
+++ py_client/basic.py
@@ -6,11 +6,11 @@ endpoint = "http://localhost:8000/api/" # "http://127.0.0.1:8000/"
 # python py_client/basic.py sadece 8000 portunda çalışıyor.
 
 # get_response = requests.get(endpoint, data={"query":
-get_response = requests.get(endpoint, json={"query":
+get_response = requests.get(endpoint, params={"abc": 123}, json={"query":
     "Hello World"})
 # print(get_response) # print source code
-print(get_response.text) # raw text response
-print(get_response.status_code)
+# print(get_response.text) # raw text response
+# print(get_response.status_code)
 
 # requests.get() # Application programming interface
 
@@ -23,6 +23,6 @@ REST API HTTP Request -> JSON (xml)
 JavaScript Object Nototion ~ Python Dict
 Jason, JSON
 """
-print(get_response.json()['message'])
-# print(get_response.json()) # raw text response
+# print(get_response.json()['message'])
+print(get_response.json()) # raw text response
 # print(get_response.status_code)
