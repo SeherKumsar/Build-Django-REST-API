d2a62d7e2ede626bf85c0adc90694b6ef29ac0f3
SeherKumsar <seherkumsar99@outlook.com>
2024-05-19 19:58:36 +0300

    Fixed files

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,13 +1,16 @@
 import requests
 
 # endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://http://localhost:8000/" # "http://127.0.0.1:8000/"
+# python py_client/basic.py sadece 8000 portunda çalışıyor.
 
 # get_response = requests.get(endpoint, data={"query":
 get_response = requests.get(endpoint, json={"query":
     "Hello World"})
 # print(get_response) # print source code
 print(get_response.text) # raw text response
+print(get_response.status_code)
 
 # requests.get() # Application programming interface
 
@@ -20,5 +23,5 @@ REST API HTTP Request -> JSON (xml)
 JavaScript Object Nototion ~ Python Dict
 Jason, JSON
 """
-print(get_response.json()) # raw text response
-print(get_response.status_code)
+# print(get_response.json()) # raw text response
+# print(get_response.status_code)
