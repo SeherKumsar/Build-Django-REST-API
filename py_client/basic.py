7c64c02b6405f01872dad2c3ea48a27df998d719
SeherKumsar <seherkumsar99@outlook.com>
2024-05-19 20:38:57 +0300

    Created first API View

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ import requests
 
 # endpoint = "https://httpbin.org/status/200/"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://http://localhost:8000/" # "http://127.0.0.1:8000/"
+endpoint = "http://localhost:8000/api/" # "http://127.0.0.1:8000/"
 # python py_client/basic.py sadece 8000 portunda çalışıyor.
 
 # get_response = requests.get(endpoint, data={"query":
@@ -23,5 +23,6 @@ REST API HTTP Request -> JSON (xml)
 JavaScript Object Nototion ~ Python Dict
 Jason, JSON
 """
+print(get_response.json()['message'])
 # print(get_response.json()) # raw text response
 # print(get_response.status_code)
