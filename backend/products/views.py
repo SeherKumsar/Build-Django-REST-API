6d8dc68b421b3c8e3b0877985bfd18d7b5319828
SeherKumsar <seherkumsar99@outlook.com>
2024-05-20 16:03:15 +0300

    added UpdateAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -40,6 +40,23 @@ class ProductListAPIView(generics.ListAPIView):
 
 product_list_view = ProductListAPIView.as_view()
 
+# Update View
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+            ## 
+
+product_update_view = ProductUpdateAPIView.as_view()
+
+# Delete View
+
+
 @api_view(['GET', 'POST'])
 def product_alt_view(request, pk=None, *args, **kwargs):
     method = request.method # PUT -> update # DESTROY -> delete
