d39a22c88ae05d44ec1cf5ab783a6f079dbc0bf3
SeherKumsar <seherkumsar99@outlook.com>
2024-05-20 18:03:32 +0300

    User & Group Permissions with DjangoModelPermissions

--- backend/products/views.py
+++ backend/products/views.py
@@ -12,7 +12,8 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.DjangoModelPermissions] # Changed RestFramework permission
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
@@ -48,6 +49,7 @@ class ProductUpdateAPIView(generics.UpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'pk'
+    # permission_classes = [permissions.DjangoModelPermissions]
 
     def perform_update(self, serializer):
         instance = serializer.save()
