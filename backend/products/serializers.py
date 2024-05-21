6b131302dfc565af166501e019ea2ef450d2483a
SeherKumsar <seherkumsar99@outlook.com>
2024-05-21 14:30:15 +0300

    Related Fields & Foreign Key Serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -2,9 +2,22 @@ from rest_framework import serializers
 from rest_framework.reverse import reverse
 from .models import Product
 from . import validators
-from . import validators
+from api.serializers import UserPublicSerializer
 
+class ProductInlineSerializer(serializers.Serializer):
+    url = serializers.HyperlinkedIdentityField(
+            view_name='product-detail',
+            lookup_field='pk',
+            read_only=True
+    )
+    title = serializers.CharField(read_only=True)
+
+# class ProductSerializer(serializers.Serializer):
 class ProductSerializer(serializers.ModelSerializer):
+    owner = UserPublicSerializer(source='user', read_only=True)
+    related_products = ProductInlineSerializer(source='user.product_set.all', read_only=True, many=True)
+    # user = UserPublicSerializer(read_only=True)
+    my_user_data = serializers.SerializerMethodField(read_only=True)
     my_discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(
@@ -13,13 +26,12 @@ class ProductSerializer(serializers.ModelSerializer):
     )
     # title = serializers.CharField(validators=[validate_title])
     title = serializers.CharField(validators=[validators.validate_title_no_hello, validators.unique_product_title])
-    # name = serializers.CharField(source="title", read_only=True)
-    # email = serializers.EmailField(ource="user.email", read_only=True)
-    # email = serializers.EmailField(write_only=True)
-    # email = serializers.EmailField()
+    # email = serializers.EmailField(source="user.email", read_only=True)
+
     class Meta:
         model = Product
         fields = [
+            'owner',
             # 'user',
             'url',
             'edit_url',
@@ -31,8 +43,14 @@ class ProductSerializer(serializers.ModelSerializer):
             'price',
             'sale_price',
             'my_discount',
+            'my_user_data',
+            'related_products',
         ]
-
+    
+    def get_my_user_data(self, obj):
+        return {
+            "username": obj.user.username
+        }
     # def validate_title(self, value): # validate_<fieldname>
     #     request = self.context.get('request')
     #     user = request.user
