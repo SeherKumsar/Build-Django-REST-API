7e8bf756e607abac14d1da89ba0d75af40ddc028
SeherKumsar <seherkumsar99@outlook.com>
2024-05-21 14:57:03 +0300

    Pagination

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -18,55 +18,29 @@ class ProductInlineSerializer(serializers.Serializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     owner = UserPublicSerializer(source='user', read_only=True)
-    related_products = ProductInlineSerializer(source='user.product_set.all', read_only=True, many=True)
-    my_user_data = serializers.SerializerMethodField(read_only=True)
-    my_discount = serializers.SerializerMethodField(read_only=True)
+    
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(
             view_name='product-detail',
             lookup_field='pk'
     )
     title = serializers.CharField(validators=[validators.validate_title_no_hello, validators.unique_product_title])
-    # email = serializers.EmailField(source='user.email', read_only=True)
     class Meta:
         model = Product
         fields = [
             'owner',
-            # 'email',
             'url',
             'edit_url',
             'pk',
             'title',
-            # 'name',
             'content',
             'price',
             'sale_price',
-            'my_discount',
-            'my_user_data',
-            'related_products',
         ]
     def get_my_user_data(self, obj):
         return {
             "username": obj.user.username
         }
-    # def validate_title(self, value):
-    #     request = self.context.get('request')
-    #     user = request.user
-    #     qs = Product.objects.filter(user=user, title__iexact=value)
-    #     if qs.exists():
-    #         raise serializers.ValidationError(f"{value} is already a product name.")
-    #     return value
-    
-    # def create(self, validated_data):
-    #     # return Product.obejcts.create(**validated_data)
-    #     # email = validated_data.pop('email')
-    #     obj = super().create(validated_data)
-    #     # print(email, obj)
-    #     return obj
-    
-    # def update(self, instance, validated_data):
-    #     email = validated_data.pop('email')
-    #     return super().update(instance, validated_data)
     
     def get_edit_url(self, obj):
         request = self.context.get('request') # self.request
