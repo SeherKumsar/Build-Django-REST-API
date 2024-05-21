ad49cf8331f01c21b8f07e4390e9bc24caeebc79
SeherKumsar <seherkumsar99@outlook.com>
2024-05-21 11:26:28 +0300

    Custom Validation with Serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 from .models import Product
+from . import validators
+# from .validators import validate_title
+from . import validators
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
@@ -9,6 +12,10 @@ class ProductSerializer(serializers.ModelSerializer):
             view_name='product-detail',
             lookup_field='pk'
     )
+    # title = serializers.CharField(validators=[validate_title])
+    title = serializers.CharField(validators=[validators.validate_title_no_hello, validators.unique_product_title])
+    # name = serializers.CharField(source="title", read_only=True)
+    # email = serializers.EmailField(ource="user.email", read_only=True)
     # email = serializers.EmailField(write_only=True)
     # email = serializers.EmailField()
     class Meta:
@@ -16,15 +23,25 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = [
             'url',
             'edit_url',
-            # 'email',
             'pk',
             'title',
+            # 'email',
+            # 'name',
             'content',
             'price',
             'sale_price',
             'my_discount',
         ]
-    
+
+    # def validate_title(self, value): # validate_<fieldname>
+    #     request = self.context.get('request')
+    #     user = request.user
+    #     qs = Product.objects.filter(user=user, title__iexact=value)
+    #     # qs = Product.objects.filter(title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f"{value} is already a product name.")
+    #     return value
+
     # def create(self, validated_data):
     #     # return Product.obejcts.create(**validated_data)
     #     # email = validated_data.pop('email')
