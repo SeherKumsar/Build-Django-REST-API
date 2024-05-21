43f73bf96288bd5ec1f255f19a56bae5e6c73baa
SeherKumsar <seherkumsar99@outlook.com>
2024-05-21 23:51:30 +0300

    Using JWT with JS Client

--- js_client/client.js
+++ js_client/client.js
@@ -1,3 +1,4 @@
+const contentContainer = document.getElementById('content-container')
 const loginForm = document.getElementById('login-form')
 const baseEndpoint = "http://localhost:8000/api"
 if (loginForm) {
@@ -22,13 +23,99 @@ function handleLogin(event) {
     }
     fetch(loginEndpoint, options) //  Promise
     .then(response=>{
-        console.log(response)
+        // console.log(response)
         return response.json()
     })
-    .then(x => {
-        console.log(x)
+    // .then(x => {
+    //     console.log(x)
+    // })
+    // .then(handleAuthData)
+    .then(authData => {
+        handleAuthData(authData, getProductList)
     })
     .catch(err=> {
         console.log('err', err)
     })
 }
+
+// function handleAuthData(authData) {
+//     localStorage.setItem('access', authData.access)
+//     localStorage.setItem('refresh', authData.refresh)
+// }
+function handleAuthData(authData, callback) {
+    localStorage.setItem('access', authData.access)
+    localStorage.setItem('refresh', authData.refresh)
+    if (callback) {
+        callback()
+    }
+}
+
+function writeToContainer(data) {
+    if (contentContainer) {
+        contentContainer.innerHTML = "<pre>" + JSON.stringify(data, null, 4) + "</pre>"
+    }
+}
+
+function getFetchOptions(method, body){
+    return {
+        method: method === null ? "GET" : method,
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem('access')}`
+        },
+        body: body ? body : null
+    }
+}
+
+function isTokenNotValid(jsonData) {
+    if (jsonData.code && jsonData.code === "token_not_valid"){
+        // run a refresh token fetch
+        alert("Please login again")
+        return false
+    }
+    return true
+}
+
+function validateJWTToken() {
+    // fetch
+    const endpoint = `${baseEndpoint}/token/verify/`
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            token: localStorage.getItem('access')
+        })
+    }
+    fetch(endpoint, options)
+    .then(response=>response.json())
+    .then(x=> {
+        // refresh token
+        // // show response
+        // console.log(x)
+        // isTokenNotValid(x)
+    })
+}
+
+function getProductList(){
+    const endpoint = `${baseEndpoint}/products/`
+    const options = getFetchOptions()
+    fetch(endpoint, options)
+    .then(response=>{
+        // console.log(response)
+        return response.json()
+    })
+    .then(data=> {
+        const validData = isTokenNotValid(data)
+        if (validData) {
+            // console.log(data)
+            // isTokenNotValid(data)
+            writeToContainer(data)
+        }
+
+    })
+}
+
+validateJWTToken()
+// getProductList()
